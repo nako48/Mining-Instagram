11e4dc43472bc64260ddde20c4b22d1450cbb49c
Arvan Apriyana <40161320+nako48@users.noreply.github.com>
2018-12-30 18:40:54 +0700

    Update mining.sh

--- mining.sh
+++ mining.sh
@@ -27,6 +27,8 @@ banner(){
 mining(){
 	getfoll && getbalance
 	ambil=$(curl -s --compressed --cookie ${random}_tmp "http://followers.interinstplus.com/api.php" -L \
+	-H 'User-Agent: okhttp/'$satudigit'.'$satudigit'.'$satudigit'.'$duadigit'' \
+	-H 'Content-Type: application/x-www-form-urlencoded' \
 	--data "method=order.get&device_id=$deviceid&device_hash=$devicehash")	
 	getdata=$(echo $ambil | grep -Po '(?<=id":)[^},]*' | tr -d '[]"' | sed 's/\(<[^>]*>\|<\/>\|{1|}\)//g')
 	username=$(echo $ambil | grep -Po '(?<=user_name":)[^},]*' | tr -d '[]"' | sed 's/\(<[^>]*>\|<\/>\|{1|}\)//g')
@@ -34,14 +36,12 @@ mining(){
 	if [[ $ambil =~ "photo_url" ]]; then
 		printf "Ambil => Balance $getbalance\n" 
 	else
-		printf "Ambil => Balance Skiped\n" 	
+		printf "Ambil => Belance Gagal\n" 	
 	fi
 }
 getfoll(){
 	satudigit=$(cat /dev/urandom | tr -dc '0-9' | fold -w 1 | head -n 1)
 	duadigit=$(cat /dev/urandom | tr -dc '0-9' | fold -w 2 | head -n 1)
-	deviceid="e16acc37a1dee996_26" #set 
-	devicehash="475aa933d1c9517177f4d1f9548b6a6f" #set
 	follow=$(curl -s 'http://followers.interinstplus.com/api.php' -H 'User-Agent: okhttp/'$satudigit'.'$satudigit'.'$satudigit'.'$duadigit'' -H 'Content-Type: application/x-www-form-urlencoded' --data "method=order.follow&device_id=$deviceid&device_hash=$devicehash&order_id=$getdata&user_id=$folowerid&skip=0&info=ok")
 }
 getbalance(){
@@ -49,6 +49,9 @@ getbalance(){
 	getbalance=$(echo $balance | grep -Po '(?<=balance":)[^},]*' | tr -d '[]"' | sed 's/\(<[^>]*>\|<\/>\|{1|}\)//g') 
 }
 banner
+read -p "Masukan DeviceID : " deviceid
+read -p "Masukan DeviceHash : " devicehash
 for (( ; ; )); do
-    mining
+    mining $deviceid $devicehash
 done
+wait
